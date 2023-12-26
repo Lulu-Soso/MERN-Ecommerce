0e9c6dc7bdd3517b45ce8a9277212be679d4e797
Lulu-Soso <lucas.sokha@gmail.com>
2023-12-26 09:29:24 +0100

    Cart Utils File

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,12 +7,6 @@ import { CssBaseline } from "@mui/material";
 import { theme } from "./theme";
 import { Provider } from 'react-redux';
 import store from "./store";
-// import { configureStore } from "@reduxjs/toolkit";
-// import cartReducer from "./state";
-
-// const store = configureStore({
-//   reducer: { cart: cartReducer }
-// })
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
